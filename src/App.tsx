ee0055c619fe4f838ea3ab48d7087b013f7bd154
JuniorManzini <manzini,junior@gmail.com>
2021-06-04 15:48:56 -0300

    terceira

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import {Dashboard} from "./components/Dashboard"
+
+export function App() {
+  return (
+    <>
+      <Header></Header>
+      <Dashboard></Dashboard>
+      <GlobalStyle></GlobalStyle>
+    </>
+  );
+}
+
+
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,9 @@
+import {Container} from './styles'
+
+export function Summary(){
+    return(
+        <Container>
+            <h1>Summary quadros1</h1>
+        </Container>
+    )
+}
