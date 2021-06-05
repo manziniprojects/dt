09a208dd6aca0bf00f2e983c4197f93d29ac9794
junior manzini <juniormanzini@iMac-de-junior.local>
2021-06-05 18:06:21 -0300

    table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,12 @@
 import { Summary } from "../Summary";
 import { Container } from "./styles";
-
+import {TransactionTable} from '../TransactionTable'
 export function Dashboard() {
 
     return (
         <Container>
             <Summary></Summary>
+            <TransactionTable></TransactionTable>
         </Container>
     )
 
