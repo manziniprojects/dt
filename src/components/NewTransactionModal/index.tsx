023b732b36a4f0a55c0931117c1e6bad8850da1c
junior manzini <juniormanzini@matcarra-RV411-RV511-E3511-S3511-RV711-E3411.localdomain>
2021-06-21 09:57:05 -0300

    modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,19 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer, RadioBox} from './styles'
 import closeImg from '../../assets/close.svg'
-
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 interface NewTransactionModalProps {
     isOpen: boolean
     onRequestClose: () => void
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    //ao armazenar informacao através do input do usuario, usar um estado
+    const [type, setType] = useState('deposit')
+
+
     return (
         <Modal isOpen={isOpen}
             onRequestClose={onRequestClose}
@@ -34,6 +40,29 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type='number'
                     placeholder='Valor'
                 />
+
+
+                <TransactionTypeContainer>
+
+                    <RadioBox type="button"
+                    onClick={()=>{setType('deposit')}}   
+                    isActive={type==='deposit'}
+                    
+                    >
+                    
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox type="button" 
+                    onClick={()=>{setType('retirada')}}
+                    isActive={type==='retirada'}
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+
+                </TransactionTypeContainer>
 
                 <input
                     placeholder='Categoria'
